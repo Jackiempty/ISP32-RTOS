abe98dab8909e3afed13f6f0f75fc3036f9d40d5
Jackiempty <candypaper0123@gmail.com>
2024-10-15 15:59:35 +0800

    Add addition algorithm to calibrate the direction of the board

--- components/sensors/imu.c
+++ components/sensors/imu.c
@@ -1,5 +1,11 @@
 #include "imu.h"
 
+static float rotation[3][3] = {
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1},
+};
+
 static calibration_t cal = {._ascale = AFS_16G,
                             ._gscale = GFS_2000DPS,
                             ._aodr = ODR_6660Hz,
@@ -9,6 +15,10 @@ static calibration_t cal = {._ascale = AFS_16G,
                             ._accelBias = {0, 0, 0},
                             ._gyroBias = {0, 0, 0}};
 static imu_t imu_instance;
+
+static inline void transform_accel_gyro(vector_t *);
+static inline void transform_mag(vector_t *);
+static inline void rotate(vector_t *);
 
 void imu_init() {
   /* Init the MPU and AHRS */
@@ -29,6 +39,13 @@ void imu_init() {
 
 void imu_update() {
   readData(&imu_instance, &cal);
+
+  transform_accel_gyro(&imu_instance.a);
+  transform_accel_gyro(&imu_instance.g);
+
+  rotate(&imu_instance.a);
+  rotate(&imu_instance.g);
+
   // Apply the AHRS algorithm
   MadgwickAHRSupdate(DEG2RAD(imu_instance.g.x), DEG2RAD(imu_instance.g.y), DEG2RAD(imu_instance.g.z), imu_instance.a.x, imu_instance.a.y,
                      imu_instance.a.z, 0, 0, 0);
@@ -40,3 +57,44 @@ void imu_update() {
 }
 
 imu_t *imu_fetch() { return &imu_instance; }
+
+/**
+ * Transformation:
+ *  - Rotate around Z axis 180 degrees
+ *  - Rotate around X axis -90 degrees
+ * @param  {object} s {x,y,z} sensor
+ * @return {object}   {x,y,z} transformed
+ */
+static inline void transform_accel_gyro(vector_t *v) {
+  float x = v->x;
+  float y = v->y;
+  float z = v->z;
+
+  v->x = -x;
+  v->y = -z;
+  v->z = -y;
+}
+
+/**
+ * Transformation: to get magnetometer aligned
+ * @param  {object} s {x,y,z} sensor
+ * @return {object}   {x,y,z} transformed
+ */
+static inline void transform_mag(vector_t *v) {
+  float x = v->x;
+  float y = v->y;
+  float z = v->z;
+
+  v->x = -y;
+  v->y = z;
+  v->z = -x;
+}
+
+static inline void rotate(vector_t *v) {
+  const float x = v->x * rotation[0][0] + v->y * rotation[0][1] + v->z * rotation[0][2];
+  const float y = v->x * rotation[1][0] + v->y * rotation[1][1] + v->z * rotation[1][2];
+  const float z = v->x * rotation[2][0] + v->y * rotation[2][1] + v->z * rotation[2][2];
+  v->x = x;
+  v->y = y;
+  v->z = z;
+}
