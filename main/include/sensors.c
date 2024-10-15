88614bade3df989a4e1da34468d760c7a805c389
Jackiempty <candypaper0123@gmail.com>
2024-10-15 19:04:00 +0800

    Adjust some timer arguments minorly

--- main/include/sensors.c
+++ main/include/sensors.c
@@ -24,14 +24,14 @@ void sensors_init() {
 void sensors_task() {
   while (1) {
     systick = bsp_current_time();
-    printf("bmp280_altitude: %f\n", pressure_altitude_instance->relative_altitude);
-    printf("bmp280_velocity: %f\n", pressure_altitude_instance->velocity);
-    printf("gps_longitude: %ld\n", gps_instance->longitude);
-    printf("gps_latitude: %ld\n", gps_instance->latitude);
-    printf("gps_altitude: %f\n", gps_instance->altitude);
-    printf("imu_accel: x: %f, y: %f, z: %f\n", imu_instance->a.x, imu_instance->a.y, imu_instance->a.z);
-    printf("imu_gyro: x: %f, y: %f, z: %f\n", imu_instance->g.x, imu_instance->g.y, imu_instance->g.z);
-    printf("----------------------------------------------\n");
+    // printf("bmp280_altitude: %f\n", pressure_altitude_instance->relative_altitude);
+    // printf("bmp280_velocity: %f\n", pressure_altitude_instance->velocity);
+    // printf("gps_longitude: %ld\n", gps_instance->longitude);
+    // printf("gps_latitude: %ld\n", gps_instance->latitude);
+    // printf("gps_altitude: %f\n", gps_instance->altitude);
+    // printf("imu_accel: x: %f, y: %f, z: %f\n", imu_instance->a.x, imu_instance->a.y, imu_instance->a.z);
+    // printf("imu_gyro: x: %f, y: %f, z: %f\n", imu_instance->g.x, imu_instance->g.y, imu_instance->g.z);
+    // printf("----------------------------------------------\n");
     comm_dump();
     ESP_LOGI(TAG, "%lu, %f, %f, %ld, %ld, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", systick, pressure_altitude_instance->relative_altitude,
              pressure_altitude_instance->velocity, gps_instance->longitude, gps_instance->latitude, gps_instance->altitude,
